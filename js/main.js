6e57ef5a0af209f0a02e1478a6e8c32ee3892396
Colleen O'Reilly <colleen@jelcreative.com>
2018-01-11 17:09:26 -0500

    background

--- js/main.js
+++ js/main.js
@@ -3,4 +3,6 @@ $(document).ready(function () {
 	// 	interval: 1000
 	// })
   console.log('linked')
+  // $('.name').animate({'top' : '10px'}, 2000);
+  // $('.name').fadeIn('100000').removeClass('hidden');
 })
