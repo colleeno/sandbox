a5ba3ead317b5641c6746ccda91cc71ad38964dd
Colleen O'Reilly <colleenaoreilly@gmail.com>
2018-01-10 22:04:06 -0500

    test swipe

--- js/main.js
+++ js/main.js
@@ -2,5 +2,39 @@ $(document).ready(function () {
   // $('.carousel').carousel({
 	// 	interval: 1000
 	// })
-  console.log('linked')
+
+  // works on scroll
+  // $('#iconCarousel').bind('mousewheel DOMMouseScroll', function(e){
+  //
+  //       if(e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {
+  //           $(this).carousel('prev');
+  //
+  //
+  //       }
+  //       else{
+  //           $(this).carousel('next');
+  //
+  //       }
+  //   });
+
+  $("#iconCarousel").on("touchstart", function(event){
+
+        var yClick = event.originalEvent.touches[0].pageY;
+    	$(this).one("touchmove", function(event){
+
+        var yMove = event.originalEvent.touches[0].pageY;
+        if( Math.floor(yClick - yMove) > 1 ){
+            $(".carousel").carousel('next');
+        }
+        else if( Math.floor(yClick - yMove) < -1 ){
+            $(".carousel").carousel('prev');
+        }
+    });
+    $(".carousel").on("touchend", function(){
+            $(this).off("touchmove");
+    });
+});
+
+});
+
 })
