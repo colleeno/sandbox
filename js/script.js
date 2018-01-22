ca5d8dc954655d6d695faaed8b7947b4eda5bec5
Colleen O'Reilly <colleen@jelcreative.com>
2018-01-22 16:13:16 -0500

    changed photo

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ console.log('linked')
 function parallaxScroll() {
     var scrolled = $(window).scrollTop();
     var faster = 0-(scrolled*.25)
+    var slower = 0-(scrolled*.15)
     $('.six-content, .about-short').css('transform','translateY('+faster+'px)')
   }
 
